7416ecb62fca17e3ca0185e27643dafb965a2744
yekkhan <syekkhan@gmail.com>
2022-09-04 20:44:11 +0800

    redis implementation

---
+++ src/main/java/com/y5n/urlshortener/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.y5n.urlshortener.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String REDIS_HOSTNAME;
+
+    @Value("${spring.redis.password}")
+    private String REDIS_PASSWORD;
+
+    @Value("${spring.redis.port}")
+    private int REDIS_PORT;
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        // Add some specific configuration here. Key serializers, etc.
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(REDIS_HOSTNAME, REDIS_PORT);
+        configuration.setPassword(REDIS_PASSWORD);
+        JedisClientConfiguration jedisClientConfiguration = JedisClientConfiguration.builder().build();
+        JedisConnectionFactory factory = new JedisConnectionFactory(configuration, jedisClientConfiguration);
+
+        factory.afterPropertiesSet();
+        redisTemplate.setConnectionFactory(factory);
+
+        return redisTemplate;
+    }
+}
